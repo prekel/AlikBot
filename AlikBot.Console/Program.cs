10d1815a22d1348f43b9bc166134ea49c8d75a71
Владислав Прекель <79232809414@yandex.ru>
2017-03-25 19:49:28 +0700

    wb1

--- AlikBot.Console/Program.cs
+++ AlikBot.Console/Program.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AlikBot.Core;
+
 namespace AlikBot.Console
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			
+			var wb = new WordBase(@"C:\Users\vladislav\OneDrive\Projects\AlikBot\AlikBot.Core\pldf-win.txt");
+			wb.InitAsync().Wait();
 		}
 	}
 }
---
+++ AlikBot.Core/WordBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlikBot.Core
+{
+	public class WordBase : List<string>
+	{
+		public Dictionary<string, int> Files;
+
+		public WordBase(params string[] files)
+		{
+			Files = new Dictionary<string, int>();
+			foreach (var i in files)
+			{
+				Files[i] = 1;
+			}
+		}
+
+		public async Task InitAsync()
+		{
+			foreach (var f in Files)
+			{
+				using (var r = new StreamReader(f.Key))
+				{
+					AddRange((await r.ReadToEndAsync()).Split());
+				}
+			}
+		}
+	}
+}
