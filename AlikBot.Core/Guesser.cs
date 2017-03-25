ac4965d8ae1cb157ba99a0e7b93cc41ec19439a6
Владислав Прекель <79232809414@yandex.ru>
2017-03-25 20:52:10 +0700

    add Guesser

---
+++ AlikBot.Core/Guesser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlikBot.Core
+{
+	public class Guesser
+	{
+		public Matcher Matcher { get; set; }
+		public WordBase Words { get; set; }
+
+		public Guesser()
+		{
+		}
+
+		public Guesser(int n) => Matcher = new Matcher(n);
+
+		public Guesser(int n, WordBase b)
+		{
+			Matcher = new Matcher(n); 
+			Words = b;
+		}
+
+		public Guesser(Matcher m, WordBase b)
+		{
+			Matcher = m;
+			Words = b;
+		}
+	}
+}
