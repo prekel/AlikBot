9ed82676e0e4f167ddb94c484e39767f2e1c197e
Владислав Прекель <79232809414@yandex.ru>
2017-03-27 23:39:43 +0700

    userinfo

---
+++ AlikBot.Telegram/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputMessageContents;
+using Telegram.Bot.Types.ReplyMarkups;
+
+using AlikBot.Core;
+
+namespace AlikBot.Telegram
+{
+	public class UserInfo
+	{
+		public static Guesser Guessers { get; set; }
+		public static bool QuantityRequest { get; set; }
+		public static bool InterviewRequest { get; set; }
+		public static char Previous { get; set; }
+	}
+}
